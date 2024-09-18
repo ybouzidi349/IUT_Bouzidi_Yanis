2de9699e5e76c0e9d1efdb0c223eb43f91a85c3c
TABLE3 <touloniut@gmail.com>
2024-09-18 18:09:42 +0200

    ok

--- Embedded/Robot_deRauglaudre_Bouzidi.X/ADC.c
+++ Embedded/Robot_deRauglaudre_Bouzidi.X/ADC.c
@@ -27,7 +27,7 @@ void InitADC1() {
     AD1CON2bits.VCFG = 0b000; // 000 : Voltage Reference = AVDD AVss
     AD1CON2bits.CSCNA = 1; // 1 : Enable Channel Scanning
     AD1CON2bits.CHPS = 0b00; // Converts CH0 only
-    AD1CON2bits.SMPI = 2; // 2+1 conversions successives avant interrupt
+    AD1CON2bits.SMPI = 4; // 2+1 conversions successives avant interrupt
     AD1CON2bits.ALTS = 0;
     AD1CON2bits.BUFM = 0;
     /************************************************************/
@@ -55,6 +55,7 @@ void InitADC1() {
     AD1CSSLbits.CSS9 = 1; // Enable AN9 for scan
     AD1CSSLbits.CSS10 = 1;
     AD1CSSLbits.CSS11 = 1; // Enable AN10 for scan
+    
     /* Assign MUXA inputs */
     AD1CHS0bits.CH0SA = 0; // CH0SA bits ignored for CH0 +ve input selection
     AD1CHS0bits.CH0NA = 0; // Select VREF- for CH0 -ve inpu
