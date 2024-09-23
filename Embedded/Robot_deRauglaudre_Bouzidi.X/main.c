09eef142ce7f24238b0c98aa89976cb148420d19
TABLE3 <touloniut@gmail.com>
2024-09-23 17:08:05 +0200

    ok

--- Embedded/Robot_deRauglaudre_Bouzidi.X/main.c
+++ Embedded/Robot_deRauglaudre_Bouzidi.X/main.c
@@ -43,82 +43,6 @@ void StateBinary(void) {
     n += BinaryCapteur(robotState.distanceTelemetreExtDroit);
     return (n);
 }
-
-void OperatingSystemLoop(void) {
-    switch (stateRobot) {
-        case 00000:
-            break;
-        case 00001:
-            break;
-        case 00010:
-            break;
-        case 00011:
-            break;
-        case 00100:
-            break;
-        case 00101:
-            break;
-        case 00110:
-            break;
-        case 00111:
-            break;
-            
-        case 01000:
-            break;
-        case 01001:
-            break;
-        case 01010:
-            break;
-        case 01011:
-            break;
-        case 01100:
-            break;
-        case 01101:
-            break;
-        case 01111:
-            break;
-        case 10000:
-            break;
-        case 10001:
-            break;
-        case 10010:
-            break;
-        case 10011:
-            break;
-        case 10100:
-            break;
-        case 10101:
-            break;
-        case 10111:
-            break;
-        case 11000:
-            break;
-        case 11001:
-            break;
-        case 11010:
-            break;
-        case 11011:
-            break;
-        case 11100:
-            break;
-        case 11101:
-            break;
-        case 11110:
-            break;
-        case 11111:
-            break;
-        case 00000:
-            break;
-        case 00000:
-            break;
-        case 00000:
-            break;
-        case 00000:
-            break;
-
-
-    }
-}
 unsigned char nextStateRobot = 0;
 
 void SetNextRobotStateInAutomaticMode() {
@@ -176,3 +100,162 @@ void Infrarouge_Conversion() {
 }
 
 
+unsigned char stateRobot;
+
+void OperatingSystemLoop(void) {
+    switch (stateRobot) {
+        case STATE_ATTENTE:
+            timestamp = 0;
+            PWMSetSpeedConsigne(0, MOTEUR_DROIT);
+            PWMSetSpeedConsigne(0, MOTEUR_GAUCHE);
+            stateRobot = STATE_ATTENTE_EN_COURS;
+        case STATE_ATTENTE_EN_COURS:
+            if (timestamp > 1000)
+                stateRobot = STATE_AVANCE;
+            break;
+        case STATE_AVANCE:
+            PWMSetSpeedConsigne(30, MOTEUR_DROIT);
+            PWMSetSpeedConsigne(30, MOTEUR_GAUCHE);
+            stateRobot = STATE_AVANCE_EN_COURS;
+            break;
+        case STATE_AVANCE_EN_COURS:
+            SetNextRobotStateInAutomaticMode();
+            break;
+        case STATE_TOURNE_GAUCHE:
+            PWMSetSpeedConsigne(30, MOTEUR_DROIT);
+            PWMSetSpeedConsigne(0, MOTEUR_GAUCHE);
+            stateRobot = STATE_TOURNE_GAUCHE_EN_COURS;
+            break;
+        case STATE_TOURNE_GAUCHE_EN_COURS:
+            SetNextRobotStateInAutomaticMode();
+            break;
+        case STATE_TOURNE_DROITE:
+            PWMSetSpeedConsigne(0, MOTEUR_DROIT);
+            PWMSetSpeedConsigne(30, MOTEUR_GAUCHE);
+            stateRobot = STATE_TOURNE_DROITE_EN_COURS;
+            break;
+        case STATE_TOURNE_DROITE_EN_COURS:
+            SetNextRobotStateInAutomaticMode();
+            break;
+        case STATE_TOURNE_SUR_PLACE_GAUCHE:
+            PWMSetSpeedConsigne(15, MOTEUR_DROIT);
+            PWMSetSpeedConsigne(-15, MOTEUR_GAUCHE);
+            stateRobot = STATE_TOURNE_SUR_PLACE_GAUCHE_EN_COURS;
+            break;
+        case STATE_TOURNE_SUR_PLACE_GAUCHE_EN_COURS:
+            SetNextRobotStateInAutomaticMode();
+            break;
+        case STATE_TOURNE_SUR_PLACE_DROITE:
+            PWMSetSpeedConsigne(-15, MOTEUR_DROIT);
+            PWMSetSpeedConsigne(15, MOTEUR_GAUCHE);
+            stateRobot = STATE_TOURNE_SUR_PLACE_DROITE_EN_COURS;
+            break;
+        case STATE_TOURNE_SUR_PLACE_DROITE_EN_COURS:
+            SetNextRobotStateInAutomaticMode();
+            break;
+        default:
+            stateRobot = STATE_ATTENTE;
+            break;
+    }
+}
+unsigned char nextStateRobot = 0;
+
+void SetNextRobotStateInAutomaticMode() {
+    unsigned char positionObstacle = PAS_D_OBSTACLE;
+    //éDtermination de la position des obstacles en fonction des ééètlmtres
+    if (robotState.distanceTelemetreDroit < 30 &&
+            robotState.distanceTelemetreCentre > 20 &&
+            robotState.distanceTelemetreGauche > 30) //Obstacle àdroite
+        positionObstacle = OBSTACLE_A_DROITE;
+    else if (robotState.distanceTelemetreDroit > 30 &&
+            robotState.distanceTelemetreCentre > 20 &&
+            robotState.distanceTelemetreGauche < 30) //Obstacle àgauche
+        positionObstacle = OBSTACLE_A_GAUCHE;
+    else if (robotState.distanceTelemetreCentre < 20) //Obstacle en face
+        positionObstacle = OBSTACLE_EN_FACE;
+    else if (robotState.distanceTelemetreDroit > 30 &&
+            robotState.distanceTelemetreCentre > 20 &&
+            robotState.distanceTelemetreGauche > 30) //pas d?obstacle
+        positionObstacle = PAS_D_OBSTACLE;
+    //éDtermination de lé?tat àvenir du robot
+    if (positionObstacle == PAS_D_OBSTACLE)
+        nextStateRobot = STATE_AVANCE;
+    else if (positionObstacle == OBSTACLE_A_DROITE)
+        nextStateRobot = STATE_TOURNE_GAUCHE;
+    else if (positionObstacle == OBSTACLE_A_GAUCHE)
+        nextStateRobot = STATE_TOURNE_DROITE;
+    else if (positionObstacle == OBSTACLE_EN_FACE)
+        nextStateRobot = STATE_TOURNE_SUR_PLACE_GAUCHE;
+    //Si l?on n?est pas dans la transition de lé?tape en cours
+    if (nextStateRobot != stateRobot - 1)
+        stateRobot = nextStateRobot;
+
+}
+
+
+
+void OperatingSystemLoop(void) {
+    switch (stateRobot) {
+        case 00000:
+            break;
+        case 00001:
+            break;
+        case 00010:
+            break;
+        case 00011:
+            break;
+        case 00100:
+            break;
+        case 00101:
+            break;
+        case 00110:
+            break;
+        case 00111:
+            break;
+        case 01000:
+            break;
+        case 01001:
+            break;
+        case 01010:
+            break;
+        case 01011:
+            break;
+        case 01100:
+            break;
+        case 01101:
+            break;
+        case 01111:
+            break;
+        case 10000:
+            break;
+        case 10001:
+            break;
+        case 10010:
+            break;
+        case 10011:
+            break;
+        case 10100:
+            break;
+        case 10101:
+            break;
+        case 10111:
+            break;
+        case 11000:
+            break;
+        case 11001:
+            break;
+        case 11010:
+            break;
+        case 11011:
+            break;
+        case 11100:
+            break;
+        case 11101:
+            break;
+        case 11110:
+            break;
+        case 11111:
+            break;
+
+    }
+}
