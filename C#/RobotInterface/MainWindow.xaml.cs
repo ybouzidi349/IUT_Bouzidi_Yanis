8f15d034c414fdb6e5cf75f5a6d6ecef117a2e90
TABLE3 <touloniut@gmail.com>
2024-10-15 17:12:46 +0200

    je commit

---
+++ C#/RobotInterface/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ExtendedSerialPort_NS;
+using System.IO.Ports;
+using System.Windows.Threading;
+
+
+namespace RobotInterface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ExtendedSerialPort serialPort1;
+        string receivedText;
+        DispatcherTimer timerAffichage;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            serialPort1 = new ExtendedSerialPort("COM13", 115200, Parity.None, 8, StopBits.One);
+            serialPort1.DataReceived += SerialPort1_DataReceived; 
+            serialPort1.Open();
+            timerAffichage = new DispatcherTimer();
+            timerAffichage.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            timerAffichage.Tick += TimerAffichage_Tick; ;
+            timerAffichage.Start();
+
+        }
+
+        private void TimerAffichage_Tick(object? sender, EventArgs e)
+        {
+            if (receivedText.Length > 0)
+                textBoxReception.Text += receivedText;
+        }
+
+        private void SerialPort1_DataReceived(object? sender, DataReceivedArgs e)
+        {
+            receivedText += serialPort1.ReadLine();
+            //receivedText += Encoding.UTF8.GetString(e.Data, 0, e.Data.Length);
+        }
+
+        private void buttonEnvoyer_Click(object sender, RoutedEventArgs e)
+        {
+            SendMessage();
+        }
+
+        private void textBoxEmission_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                SendMessage();
+        }
+        private void SendMessage()
+        {
+            string value = textBoxEmission.Text;
+            textBoxEmission.Text = "";
+            if (value.Length > 0)
+                serialPort1.WriteLine(value);
+
+        }
+        private void buttonClear_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxReception.Text = "";
+        }
+    }
+}
