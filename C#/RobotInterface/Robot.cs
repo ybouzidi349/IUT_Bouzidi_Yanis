f9a960ffcd468f41021bac09560fdf0a3bfe841f
TABLE3 <touloniut@gmail.com>
2024-10-15 17:59:32 +0200

    ok

---
+++ C#/RobotInterface/Robot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Robot
+{
+    public string receivedText = "";
+    public float distanceTelemetreDroit;
+    public float distanceTelemetreCentre;
+    public float distanceTelemetreGauche;
+    public Robot()
+    {
+    }
+}
+²   
